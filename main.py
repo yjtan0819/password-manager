1e6a1f10fc5bd8da21877ff45e71b46e0e175546
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 18:36:13 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from add import add
+from view import view
+
 password = input("Enter master password: ")
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones? Press q to quit. ").lower()
+    mode = input("Would you like to add a new password or view existing ones (add/view)? Press q to quit. ").lower()
 
     if mode == "q":
         break
 
     if mode == "add":
-
+        add()
     elif mode == "view":
-        
+        view()
+
     else:
         print("Invalid mode")
         continue
