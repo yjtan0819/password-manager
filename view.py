379834c6a0649574622fb49a0c922ff2c9fb4d3c
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 18:36:28 -0500

    Added view feature

--- view.py
+++ view.py
@@ -1,3 +1,6 @@
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            website, user, passw = line.rstrip().split("|")
+            print("Website: ", website, "Username: ", user, "Password: ", passw)
