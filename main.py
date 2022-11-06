86a5bb7828ddb2a211d69c81ed1dbef6d4ee2275
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 18:25:04 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+password = input("Enter master password: ")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones? Press q to quit. ").lower()
+
+    if mode == "q":
+        break
+
+    if mode == "add":
+
+    elif mode == "view":
+        
+    else:
+        print("Invalid mode")
+        continue
+    
