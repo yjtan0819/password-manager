b00422868472e986e3cc7eaeeeba1fcca44477a2
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 18:36:03 -0500

    Add feature to add password

--- add.py
+++ add.py
@@ -1,3 +1,8 @@
 
 def add():
-    pass
+    website = input("Enter the website: ")
+    username = input("Enter the username: ")
+    password = input("Enter the password: ")
+
+    with open("passwords.txt", "a") as file:
+        file.write(website + " | " + username + " | " + password + '\n')
