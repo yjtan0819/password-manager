974a28246026e2807e002e7fe100482114e66412
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 20:24:28 -0500

    Updated add.py

--- add.py
+++ add.py
@@ -1,8 +1,9 @@
+from cryptography.fernet import Fernet
 
-def add():
+def add(fer):
     website = input("Enter the website: ")
     username = input("Enter the username: ")
     password = input("Enter the password: ")
 
     with open("passwords.txt", "a") as file:
-        file.write(website + " | " + username + " | " + password + '\n')
+        file.write(website + " | " + username + " | " + fer.encrypt(password.encode()).decode() + '\n')
