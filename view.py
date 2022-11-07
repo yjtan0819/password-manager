72c3bc78fb2898e07e10072fc99ce38d849d208d
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-11-06 20:28:10 -0500

    Updated view.py

--- view.py
+++ view.py
@@ -1,6 +1,11 @@
+from cryptography.fernet import Fernet
+import os.path
 
-def view():
+def view(fer):
+    if not os.path.exists('passwords.txt'):
+        print("No passwords saved yet")
+        return
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             website, user, passw = line.rstrip().split("|")
-            print("Website: ", website, "Username: ", user, "Password: ", passw)
+            print("Website: ", website, "Username: ", user, "Password: ", fer.decrypt(passw.encode()).decode())
